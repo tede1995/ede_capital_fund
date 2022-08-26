59bc648df6dd98b0f4a67c07bdc3639385813681
Tim Ede <timothy.ede@cloud-equity.com>
2022-08-26 06:20:48 +0100

    Contact form and backend created

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .forms import ContactForm
+import logging
 
 # Create your views here.
 
 def index(request):
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			form.save()
 
-	context = {
+	form = ContactForm()
+	context = {'form': form}
 
-	}
-
-	return render(request, 'mainpage/index.html', context=context)
+	return render(request, 'mainpage/index.html', context)
