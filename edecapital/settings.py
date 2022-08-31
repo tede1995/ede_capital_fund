3eefab800f1a4fc35509d736e8116ac5c4511fd5
Tim Ede <timothy.ede@cloud-equity.com>
2022-08-31 21:31:16 +0100

    ALLOWED_HOSTS updated

--- edecapital/settings.py
+++ edecapital/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '', '']
 
 # Application definition
 
