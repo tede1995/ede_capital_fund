49fe77988e2db067e39292ff9aab3e1151a14374
Tim Ede <timothy.ede@cloud-equity.com>
2022-08-31 21:32:49 +0100

    ALLOWED_HOSTS added

--- edecapital/settings.py
+++ edecapital/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', '', '']
+ALLOWED_HOSTS = ['127.0.0.1', 'edecapital.herokuapp.com', 'edecapital.com.au']
 
 # Application definition
 
