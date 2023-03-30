173b924c69744f7fa02dc68e7717f56b6cf4f033
Tim Ede <timothy.ede@cloud-equity.com>
2023-03-30 20:06:05 +0100

    Updated STATICROOT

--- edecapital/settings.py
+++ edecapital/settings.py
@@ -127,10 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
